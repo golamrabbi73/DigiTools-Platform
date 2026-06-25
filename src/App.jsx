4a7ed4a32c4373def8b019b463dfcaa9a87a54e9
golamrabbi73 <golamrabbi39hr@gmail.com>
2026-06-25 20:44:11 +0600

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <button className="btn btn-primary">DaisyUI Test</button>
+    </div>
+  );
+}
+export default App;
